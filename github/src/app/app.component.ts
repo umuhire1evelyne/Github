6a7564c9f345ac654f67442dbed75ac5e0a30386
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-08-30 13:14:25 +0200

    app.ts codes are added

--- github/src/app/app.component.ts
+++ github/src/app/app.component.ts
@@ -1,10 +1,21 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'github';
+// }
 import { Component } from '@angular/core';
+import {GithubComponent } from './component/github.component';
+import {GithubService } from './services/github.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  title = 'github';
+export class AppComponent  {
 }
